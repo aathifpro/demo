11cf22ba8ba90fe33e2d0fafb108585811ce990d
aathifpro <aathifprofession@gmail.com>
2024-01-01 23:05:00 +0530

    payment toggle function added

--- src/app/component/payment-tag/payment-tag.component.ts
+++ src/app/component/payment-tag/payment-tag.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-payment-tag',
   templateUrl: './payment-tag.component.html',
   styleUrls: ['./payment-tag.component.css']
 })
-export class PaymentTagComponent {
+export class PaymentTagComponent implements OnInit {
+
+  ngOnInit(): void {
+    
+  }
+
+  paymentList: { name: string; visible: boolean }[] = [
+    { name: 'cashPayment', visible: false },
+    { name: 'cardPayment', visible: false },
+  ];
+
+  // Method to update payment visibility
+  visiblePayment(selectedPayment: string): void {
+    // Set the visibility of the selected payment to true
+    this.paymentList.forEach(payment => {
+      if(payment.name === selectedPayment) {
+        payment.visible = !payment.visible;
+      }
+    });
+  }
 
 }
--- src/app/screen/menu-screen/menu-screen.component.ts
+++ src/app/screen/menu-screen/menu-screen.component.ts
@@ -21,22 +21,5 @@ export class MenuScreenComponent implements OnInit {
 
   public visibleTitle: boolean = false;
 
-  categoryList: { name: string; visible: boolean }[] = [
-    { name: 'Breakfast', visible: true },
-    { name: 'Lunch', visible: false },
-    { name: 'Dinner', visible: false },
-    { name: 'Drinks', visible: false },
-    { name: 'Dessert', visible: false },
-    { name: 'Wine', visible: false },
-  ];
-
-  // Method to update category visibility
-  visibleCategory(selectedCategory: string): void {
-    // Set the visibility of the selected category to true
-    this.categoryList.forEach(category => {
-      category.visible = category.name === selectedCategory;
-    });
-  }
-
 
 }
